72fb0d9ce001b8115c737dc172222d17df0b9cdc
VanAntonietti <v.antonietti@icloud.com>
2022-02-27 19:13:23 -0300

    Makefile and Header

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,17 +6,22 @@
 /*   By: vantonie <vantonie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 18:57:26 by vantonie          #+#    #+#             */
-/*   Updated: 2022/02/27 18:57:59 by vantonie         ###   ########.fr       */
+/*   Updated: 2022/02/27 19:13:11 by vantonie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void push_swap(t_swap *swap)
+{
+	
+}
+
 int	main()
 {
 	t_swap *swap;
 
-	
+	push_swap(&swap);
 	
 	return 0;
 }
