b678ff6eac82a4fc04549e3a864028aecaebe8b2
VanAntonietti <v.antonietti@icloud.com>
2022-04-02 15:52:41 -0300

    Finished

---
+++ src/main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vantonie <vantonie@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/27 18:57:26 by vantonie          #+#    #+#             */
+/*   Updated: 2022/04/02 15:51:27 by vantonie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	t_ps	*ps;
+
+	if (argc > 1)
+	{
+		ps = malloc(sizeof(t_ps));
+		ps->t_argc = argc;
+		ps->t_argv = argv;
+		init_struct(ps);
+		if (checker_before(ps) == 0)
+		{
+			init_list(ps);
+			if (checker_after(ps) == 0 && normalize(ps) == 0)
+			{
+				push_swap(ps);
+			}
+			finalize_struct(ps);
+		}
+		free(ps);
+	}
+	return (0);
+}
